168f103ab2ded1a3c429a7f8f785c6b3ee652873
Max Harlow <maxharlow@gmail.com>
2014-10-19 16:50:47 +0100

    Scrape Labour Market Statistics from the ONS

---
+++ README.md
@@ -0,0 +1,4 @@
+Scrape ONS Labour Market Statistics
+===================================
+
+From here: http://www.ons.gov.uk/ons/rel/lms/labour-market-statistics/
---
+++ ons-labour.js
@@ -0,0 +1,25 @@
+var highland = require('highland')
+var request = require('request')
+var fs = require('fs')
+var moment = require('moment')
+var csvParser = require('csv-parser')
+var csvWriter = require('csv-write-stream')
+
+highland(request('http://www.ons.gov.uk/ons/datasets-and-tables/downloads/csv.csv?dataset=lms'))
+    .through(csvParser())
+    .filter(function (record) {
+    	return /20[0-9][0-9] [JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC]/.test(record[''])
+    })
+    .map(function (record) {
+	return {
+	    'date': moment(record[''], 'YYYY MMM').format(),
+	    'claimantCount': record['AGLX'], // UK Claimant Count - total computerised claims - People - SA (000s)
+	    'unemploymentRate': record['MGSX'], // LFS: Unemployment rate: UK: All: Aged 16 and over: %: SA
+	    'workHours': record['YBUV'] // LFS: Avg actual weekly hours of work: UK: All workers in main & 2nd job: SA
+	}
+    })
+    .through(csvWriter())
+//    .pipe(fs.createWriteStream('ons-labour.csv'))
+    .each(function (record) {
+	console.log(record.replace('\n', ''))
+    })
