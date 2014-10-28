e89cbcaed2c1c9943a22c55901f80912cd421fc8
Max Harlow <maxharlow@gmail.com>
2014-10-28 12:26:26 +0000

    Real values by claimant count

--- ons-labour.js
+++ ons-labour.js
@@ -15,7 +15,7 @@ highland(request('http://www.ons.gov.uk/ons/datasets-and-tables/downloads/csv.cs
     .map(function (record) {
 	return {
 	    'date': moment(record[''], 'YYYY MMM').format(),
-	    'claimantCount': record['AGLX'], // UK Claimant Count - total computerised claims - People - SA (000s)
+	    'claimantCount': record['AGLX'] * 1000, // UK Claimant Count - total computerised claims - People - SA (000s)
 	    'unemploymentRate': record['MGSX'], // LFS: Unemployment rate: UK: All: Aged 16 and over: %: SA
 	    'workHours': record['YBUV'] // LFS: Avg actual weekly hours of work: UK: All workers in main & 2nd job: SA
 	}
